b1ea42ca7692b1daae28b38f5e49a93381d4e4cf
Eurico F. Pedrosa <efp@ua.pt>
2020-05-09 18:26:05 +0100

    Add zshell install.
    This will install zsh with oh-my-zsh and powerlevel10k.

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ set -o errexit
 set -o nounset
 
 function install_sys_bin {
-    local PACKAGES='git neovim python3-neovim python3-jedi tmux'
+    local PACKAGES='git neovim python3-neovim python3-jedi zsh curl tmux'
 
     echo -e "\033[0;32mInstalling necessary packages...\033[0m"
     if [[ $EUID -ne 0 ]]; then
@@ -38,10 +38,40 @@ function install_neovim {
     vi -c "PlugInstall" -c "qa"
 }
 
+function install_zsh {
+    echo -e "\033[0;32mInstalling zshel (with ohmyzsh and powerlevel10k)\033[0m"
+
+
+    local SRC_ZSH="$1/zsh/zshrc"
+    local DST_ZSH="$HOME/.zshrc"
+    if [[ -f $DST_ZSH ]]; then
+        echo -e "\033[0;33mThe dotfile '.zshrc' already exist, will create a backup.\033[0m"
+        mv "$DST_ZSH" "$DST_ZSH.backup"
+    fi
+
+    local SRC_P10K="$1/zsh/p10k.zsh"
+    local DST_P10K="$HOME/.config/p10k/p10k.zsh"
+    if [[ -f $DST_P10K ]]; then
+        echo -e "\033[0;33mThe dotfile 'p10k.zsh' already exist, will create a backup.\033[0m"
+        mv "$DST_P10K" "$DST_P10K.backup"
+    fi
+
+    mkdir -p $HOME/.config/p10k
+    ln -s $SRC_ZSH $DST_ZSH
+    ln -s $SRC_P10K $DST_P10K
+
+    local ZSH=$HOME/.config/oh-my-zsh
+    if [[ ! -d $ZSH ]]; then
+        sh -c "ZSH=$ZSH;RUNZSH=no;KEEP_ZSHRC=yes;$(curl -fLo https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH/custom/themes/powerlevel10k
+    fi
+}
+
 install_sys_bin
 
 REPO="https://github.com/eupedrosa/insta.dot.git"
 BASE="$HOME/.local/insta.dot"
 fetch_repo $REPO $BASE
 install_neovim $BASE
+install_zsh $BASE
 
